dc6e4f6b4b39022fcd25a35baf283be15aea97f6
Anthony Li <tongning05@gmail.com>
2015-11-07 23:58:04 -0500

    update for sql injection

--- login.php
+++ login.php
@@ -19,9 +19,16 @@ else{
 	$username=trim($_POST['username']);
 	$password=$_POST['password'];
 	$sql = "SELECT * FROM users WHERE user_name='".$username."' AND user_pass='".md5($password)."';";
+	
 	$result = mysqli_query($conn, $sql);
+	if(!$result){
+		echo "An unexpected database error occurred while logging in. Please try again later. <br><br>";
+		echo "Message from SQL: ".mysqli_error($conn);
+		echo "<br><br>";
+	}
 	if (mysqli_num_rows($result) > 0){
-		$_SESSION['login_username']=$username;
+		$row=mysqli_fetch_assoc($result);
+		$_SESSION['login_username']=$row['user_name'];
 		echo "Login successful. Click <a href='/'>here</a> to return home.";
 	}
 	else{
